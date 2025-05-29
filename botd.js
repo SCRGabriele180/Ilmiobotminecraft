048e7d74be248085fb1db18b6604219e1f43035c
SCRGabriele180 <111132844+SCRGabriele180@users.noreply.github.com>
2025-05-29 20:30:57 +0200

    Add files via upload

---
+++ botd.js
@@ -0,0 +1,29 @@
+const mineflayer = require('mineflayer');
+
+function createBot() {
+  const bot = mineflayer.createBot({
+    host: 'CansasMC.aternos.me', // Cambia con l'IP del tuo server
+    port: 30113,                   // Cambia se usi una porta diversa
+    username: 'lucailbot',        // Nome del bot (può essere qualsiasi)
+    version: '1.19.2' // forza questa versione  
+  });
+
+  bot.on('spawn', () => {
+    console.log("✅ Il bot si è connesso al server!");
+  });
+
+  bot.on('chat', (username, message) => {
+    if (username === bot.username) return;
+
+    if (message === 'ciao') {
+      bot.chat(`Ciao ${username}!`);
+    }
+  });
+
+  bot.on('end', () => {
+    console.log("⚠️ Disconnesso. Riconnessione in 30 secondi...");
+    setTimeout(createBot, 30000); // Riconnessione automatica ogni 30s
+  });
+}
+
+createBot();
